b5eb3c1fea9ccb303068570cba4e864a4d111b1c
Asonganyi Rouclec <81648176+Rouclec@users.noreply.github.com>
2025-07-04 06:37:58 +0100

    Updated CreateApplicant method (#8)
    * added level name to CreateApplicantRequest struct

--- client.go
+++ client.go
@@ -174,6 +174,7 @@ type (
 	}
 
 	CreateApplicantRequest struct {
+		LevelName      string
 		FixedInfo      FixedInfo
 		ExternalUserID string
 		Email          string
@@ -605,7 +606,7 @@ func (c *Client) ApplicantData(ctx context.Context, req ApplicantDataRequest) (A
 func (c *Client) CreateApplicant(ctx context.Context, req CreateApplicantRequest) (CreateApplicantResponse, error) {
 	resp, err := call[reqCreateApplicant, respCreateApplicant](ctx, c,
 		http.MethodPost,
-		"/resources/applicants",
+		fmt.Sprintf("/resources/applicants?levelName=%s", url.QueryEscape(req.LevelName)),
 		reqCreateApplicant{
 			FixedInfo: struct {
 				FirstName string `json:"firstName"`
